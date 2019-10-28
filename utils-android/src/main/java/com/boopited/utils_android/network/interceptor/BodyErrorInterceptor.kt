10bf7c94ef09e589f139e85e0b5822044b5426a0
Le Xing <lxing@thoughtworks.com>
2019-10-28 10:52:56 +0800

    [Le] network adapters

---
+++ utils-android/src/main/java/com/boopited/utils_android/network/interceptor/BodyErrorInterceptor.kt
@@ -0,0 +1,23 @@
+package com.boopited.utils_android.network.interceptor
+
+import okhttp3.Response
+import com.boopited.utils_android.network.model.ApiException
+import org.json.JSONObject
+
+class BodyErrorInterceptor : ResponseBodyInterceptor() {
+
+    override fun intercept(response: Response, url: String, body: String): Response {
+        var jsonObject: JSONObject? = null
+        try {
+            jsonObject = JSONObject(body)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        if (jsonObject != null) {
+            if (jsonObject.optInt("code", -1) != 200 && jsonObject.has("msg")) {
+                throw ApiException(jsonObject.getString("msg"))
+            }
+        }
+        return response
+    }
+}
---
+++ utils-android/src/main/java/com/boopited/utils_android/network/model/ApiException.kt
@@ -0,0 +1,5 @@
+package com.boopited.utils_android.network.model
+
+import java.lang.Exception
+
+class ApiException(message: String): Exception(message)
